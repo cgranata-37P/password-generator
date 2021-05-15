1a94c1ffe7770528e26822ad2b2f6d7287415c6d
Chris Granata <christophergranata@gmail.com>
2021-05-15 08:27:07 -0400

    Random funtion and for loop

--- script.js
+++ script.js
@@ -14,57 +14,71 @@
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 
-
-
 // Assignment Code
+// References our HTML button
 var generateBtn = document.querySelector("#generate");
+// Creating Arrays and values
 var specialChar= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">", "?"];
 var numberChar= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-userInput();
 
-function userInput() {
-  var length=prompt("How many characters would you like your password to be between 8 and 128?");
-  if (isNaN(length) === true) {
+
+function generatePassword() {
+  var passwordLength=prompt("Choose the length of your password between 8 and 128 characters long.");
+  if (isNaN(passwordLength) === true) {
     alert("The passsword length must be a number");
     return;
   };
-  if (length < 8 || length > 128){
+  if (passwordLength < 8 || passwordLength > 128){
     alert("The password length must be between 8 and 128 characters");
-    return;
+    return ;
   }
+
   var useSpecial=confirm("Would you like to use special characters?");
   var useNumber=confirm("Would you like to use numbers?");
   var useLower=confirm("Would you like to you lowercase letter?");
   var useUpper=confirm("Would you like to use uppercase letters?");
 
-  var userChoices = {
-    lenght: length,
-    useSpecial: useSpecial,
-    useNumber: useNumber,
-    useLower: useLower,
-    useUpper: useUpper
+  var approvedArray = [];
+
+  if (useSpecial) {
+    approvedArray = approvedArray.concat(specialChar);
+  }
+  if (useNumber) {
+    approvedArray = approvedArray.concat(numberChar);
+  }
+  if (useLower) {
+    approvedArray = approvedArray.concat(lowerChar);
+  }
+  if (useUpper) {
+    approvedArray = approvedArray.concat(upperChar);
   }
 
-  return userChoices;
+  var newPassword = "";
+
+  for (var i=0; i < passwordLength; i++){
+    var character = getRandom(approvedArray);
+    newPassword = newPassword + character;  
+  }
+  
+ return newPassword;
 }
 
-
-
-
-
-
-
-
+// Returns a random item from an array
+function getRandom(array) {
+  var arrayLength = array.length;
+  var random = Math.floor(Math.random() * (arrayLength));
+  return array[random];
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.value = password;
 
 }
 
